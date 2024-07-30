1388749bdc04b9b5c7a929b6f5c754d4e9605bb5
muhammadabubakar9412 <muhammadabubakar9412@gmail.com>
2024-07-30 19:48:14 +0500

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,19 @@
+let result = document.getElementById("result");
+function insertvalue(value) {
+  result.value += value;
+}
+function clearresult() {
+  result.value = "";
+}
+
+function deleteresult() {
+  result.value = result.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch (e) {
+    result.value = "Error";
+  }
+}
